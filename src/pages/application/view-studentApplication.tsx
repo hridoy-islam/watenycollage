3c5ae64ee7b2a32091241d56e96a846ce65d46f9
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-09-11 15:59:29 +0600

    add loader for course update

--- src/pages/application/view-studentApplication.tsx
+++ src/pages/application/view-studentApplication.tsx
@@ -185,7 +185,7 @@ export default function ViewStudentApplicationPage() {
   const [currentEditingCourseId, setCurrentEditingCourseId] = useState<
     string | null
   >(null); // To track which course is being edited
-
+const [isUpdatingCourse, setIsUpdatingCourse] = useState(false);
   useEffect(() => {
     const fetchApplication = async () => {
       try {
@@ -291,7 +291,7 @@ export default function ViewStudentApplicationPage() {
       });
       return;
     }
-
+setIsUpdatingCourse(true);
     try {
       await axiosInstance.patch(
         `/application-course/${currentEditingCourseId}`,
@@ -309,7 +309,9 @@ export default function ViewStudentApplicationPage() {
         title: 'Failed to update course details.',
         className: 'bg-destructive text-white border-none'
       });
-    }
+    }finally {
+    setIsUpdatingCourse(false); 
+  }
   };
 
   const copyToClipboard = (value: string, field: string) => {
@@ -1388,7 +1390,26 @@ export default function ViewStudentApplicationPage() {
 
                                   className='bg-watney text-white hover:bg-watney/90'
                                 >
-                                  Save Changes
+                                 {isUpdatingCourse ? (
+  <span className="flex items-center gap-2">
+    <svg
+      className="w-5 h-5 animate-spin text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      role="img"
+    >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+    </svg>
+
+    <span>Saving...</span>
+  </span>
+) : (
+  'Save Changes'
+)}
+
                                 </Button>
                               </DialogFooter>
                             </DialogContent>
