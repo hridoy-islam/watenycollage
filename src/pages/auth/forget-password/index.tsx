2619cbafbffec1896585af222be75538cdd1c44c
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-09-11 17:17:29 +0600

    email fix

--- src/pages/auth/forget-password/index.tsx
+++ src/pages/auth/forget-password/index.tsx
@@ -44,7 +44,7 @@ export default function ForgotPassword() {
 
     const result: any = await dispatch(requestOtp(normalizedData));
     if (result?.payload?.success) {
-      localStorage.setItem('tp_otp_email', data.email);
+      localStorage.setItem('tp_otp_email', data.email.toLowerCase());
 
       router.push('/otp');
     }
